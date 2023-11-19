918bfbcb3d3cb75462887d4a84e3c35e65b9bfb3
pabloUrizar <73521330+pabloUrizar@users.noreply.github.com>
2023-11-19 13:03:42 +0100

    Step 3 done

--- Labo5/lambda_function.py
+++ Labo5/lambda_function.py
@@ -8,7 +8,7 @@ import datetime
 def upload_file(content, bucket, object_name):
     # Upload the file
     s3_client = boto3.client('s3')
-    
+
     try:
         s3_client.put_object(Body=content, Bucket=bucket, Key=object_name)
         logging.info(f"File uploaded to S3: {object_name}")
@@ -19,11 +19,14 @@ def upload_file(content, bucket, object_name):
 
 def lambda_handler(event, context):
     logging.info("Function invoked with event %s", json.dumps(event))
-    
-    datetimenow = datetime.datetime.now().replace(microsecond=0).isoformat()
+
+    # Generate the object name with the desired naming convention
+    datetimenow = datetime.datetime.now().replace(microsecond=0).strftime("%Y-%m-%dT%H:%M")
+    object_name = f"current/VQHA80-{datetimenow}.csv"
+
     response = requests.get('https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/VQHA80.csv')
 
-    if upload_file(response.content, "ist-meteo-grd-urizar-valzino", "current/data-meteo-" + datetimenow + ".csv"):
+    if upload_file(response.content, "ist-meteo-grd-urizar-valzino", object_name):
         return {
             'statusCode': 200,
             'body': json.dumps('File Uploaded Successfully!')
