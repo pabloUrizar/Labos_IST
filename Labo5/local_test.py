b1bbab07275beacec4640abd40825722d1bd7e63
pabloUrizar <73521330+pabloUrizar@users.noreply.github.com>
2023-12-08 10:31:05 +0100

    Comments

--- Labo5/local_test.py
+++ Labo5/local_test.py
@@ -1,7 +1,7 @@
 import json
 from lambda_function import lambda_handler
 
-# Simulate an event, you can adjust this based on your actual event structure
+# Simulate AWS Event
 sample_event = {
     "key1": "value1",
     "key2": "value2",
@@ -11,6 +11,5 @@ sample_event = {
 # Call the Lambda handler function
 result = lambda_handler(sample_event, None)
 
-# Print the result (you can customize this based on your expected return format)
+# Print the result
 print(json.dumps(result, indent=2))
-
